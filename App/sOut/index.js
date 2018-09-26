e00ce838440c9f316f84907e8b306213aed9522b
palomara <palomarangelll@hotmail.com>
2018-09-26 19:27:28 -0300

    modifications

--- App/sOut/index.js
+++ App/sOut/index.js
@@ -4,15 +4,22 @@ import {AppRegistry} from 'react-native';
 
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
+<<<<<<< HEAD
 import Hall from './src/screens/Hall';
 import RegisterFirst from "./src/screens/RegisterFirst";
 
+=======
+>>>>>>> parent of 6a33dcf... hall init
 
 import {name as appName} from './app.json';
 
 
 //AppRegistry.registerComponent(appName, () => Home);
+<<<<<<< HEAD
 //AppRegistry.registerComponent(appName, () => Login);
 //AppRegistry.registerComponent(appName, () => Hall);
 AppRegistry.registerComponent(appName, () => RegisterFirst);
 
+=======
+AppRegistry.registerComponent(appName, () => Login);
+>>>>>>> parent of 6a33dcf... hall init
