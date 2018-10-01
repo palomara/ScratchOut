1e6bb63cfbb0b743505b75fde15137398d23aee3
Luis Lopes <luislopes116@outlook.com>
2018-09-30 21:57:34 -0300

    login Facebook chkd

--- App/sOut/android/app/src/main/java/com/sout/MainApplication.java
+++ App/sOut/android/app/src/main/java/com/sout/MainApplication.java
@@ -3,15 +3,25 @@ package com.sout;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager)
       );
     }
 
--- App/sOut/src/screens/Home.js
+++ App/sOut/src/screens/Home.js
@@ -1,41 +1,13 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-import {Dimensions, Platform, StyleSheet, ImageBackground, Image, Text, View} from 'react-native';
-import { StackNavigator, TabNavigator } from "react-navigation";
-
-
-
-const width = Dimensions.get('screen').width;
-const height = Dimensions.get('screen').height;
-=======
 import {Platform, StyleSheet, Text, View} from 'react-native';
->>>>>>> e5975c3293aee06f8b5efd7daee8a9a3250994bc
-
 
 export default class Home extends Component<Props> {
 
   render() {
     return (
-<<<<<<< HEAD
-        <View style={{ flex: 1, width: 360, height: 60 }}>
-            <ImageBackground source={require('../../resources/images/green-galaxy.png')} style={styles.background} opacity={0.16}>
-
-            <View style={styles.container}>
-
-            </View>
-                <View>
-
-                </View>
-            </ImageBackground>
-        </View>
-
-
-
-=======
       <View style={styles.container}>
         <Text>Login Efetuado!</Text>
       </View>
->>>>>>> e5975c3293aee06f8b5efd7daee8a9a3250994bc
     );
   }
 }
