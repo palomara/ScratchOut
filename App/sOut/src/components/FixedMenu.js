af6863508e695a12f570cffdf3e5327df232b24a
palomara <palomarangelll@hotmail.com>
2018-10-09 16:08:47 -0300

    createTask modal

--- App/sOut/src/components/FixedMenu.js
+++ App/sOut/src/components/FixedMenu.js
@@ -2,19 +2,20 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Image,
-  TouchableOpacity,
-  AsyncStorage,
-  ScrollView,
-  StatusBar
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    Image,
+    TouchableOpacity,
+    AsyncStorage,
+    ScrollView,
+    StatusBar, TextInput
 } from 'react-native';
 
 import Modal from "react-native-modal";
+import {login} from "./func";
 
 const width = Dimensions.get('screen').width;
 const height = Dimensions.get('screen').height;
@@ -54,12 +55,27 @@ export default class FixedMenu extends Component {
             <Image source={require('../../resources/images/icons/icon-profile.png')}/>
           </TouchableOpacity>
 
-            <Modal isVisible={this.state.isModalVisible} style={styles.modalContent}>
-                <View style={{ flex: 1 }}>
-                    <Text>Conteudo</Text>
+            <Modal isVisible={this.state.isModalVisible}>
+                <View style={styles.modalContent}>
+                    <Text style={styles.title}>Nova tarefa</Text>
+                    <Text style={styles.taskTitle}>Título</Text>
+                    <View style={styles.inputFieldPass} >
+                        <TextInput style={styles.input} />
+                        <TouchableOpacity style={styles.addTaskIcon}>
+                            <Image source={require('../../resources/images/icons/icon-new_task.png')}/>
+                        </TouchableOpacity>
+                    </View>
+
                     <TouchableOpacity onPress={this._toggleModal}>
                         <Text>Fechar</Text>
                     </TouchableOpacity>
+
+                    <View style={styles.Create}>
+                        <TouchableOpacity style={styles.createTouch}>
+                            <Text style={styles.createText}>Criar</Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
             </Modal>
 
@@ -87,11 +103,63 @@ const styles = StyleSheet.create({
 
     modalContent: {
         backgroundColor: 'white',
-        padding: 22,
+        padding: 20,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 4,
+        borderRadius: 30,
         borderColor: 'rgba(0, 0, 0, 0.1)'
+    },
+
+    title:{
+        fontFamily: 'Roboto',
+        fontSize: 20,
+        color: '#00ED74',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    taskTitle: {
+      marginTop: 10,
+      marginRight: 280,
+      fontSize: 16,
+      alignItems: 'center'
+    },
+    input: {
+        fontFamily: 'Roboto Light',
+        fontSize: 20,
+        color: '#00ED74',
+        flex: 1
+    },
+    addTaskIcon: {
+        width: 20,
+        height: 20,
+        marginRight: width * 0.1
+    },
+    inputFieldPass:{
+        borderBottomWidth: 2,
+        borderColor: '#00ED74',
+        alignItems:'center',
+        flexDirection: 'row',
+        marginBottom: 8,
+
+    },
+    Create : {
+      width: width * 0.88,
+        height: height / 12.8,
+        backgroundColor: "#00ED74",
+        borderRadius: 40,
+        marginTop: height / 50
+  },
+    createTouch: {
+        flex: 1,
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+    createText: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        color: '#fff',
+        alignItems: 'center'
     }
+
 
 });
