eb27eb92b6cd31973e85ad1235a4d8b815bbeb21
palomara <palomarangelll@hotmail.com>
2018-10-09 17:14:11 -0300

    list screen init

--- App/sOut/index.js
+++ App/sOut/index.js
@@ -7,13 +7,16 @@ import Login from './src/screens/Login';
 import Profile from './src/screens/Profile';
 import Hall from './src/screens/Hall';
 import RegisterFirst from "./src/screens/RegisterFirst";
+import TasksList from './src/screens/TasksList';
 import App from './src/App'
 
 import {name as appName} from './app.json';
 
-//AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent(appName, () => Home);
 //AppRegistry.registerComponent(appName, () => Login);
 //AppRegistry.registerComponent(appName, () => Hall);
 //AppRegistry.registerComponent(appName, () => Profile);
 //AppRegistry.registerComponent(appName, () => RegisterFirst);
+//AppRegistry.registerComponent(appName, () => TasksList);
+
--- App/sOut/src/screens/TasksList.js
+++ App/sOut/src/screens/TasksList.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
+    View,
+    Text,
+    StyleSheet, StatusBar, TouchableOpacity, Image, Dimensions,
 } from 'react-native';
+
+
+import FixedMenu from "../components/FixedMenu";
+
+const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
 
 export default class TasksList extends Component {
   render() {
     return (
       <View style={styles.container}>
+          <StatusBar backgroundColor='transparent' barStyle='dark-content'/>
+          <View style={styles.fixedNav}>
+              <TouchableOpacity style={styles.fixedNavArea} onPress={() => console.warn("Back to Home")}>
+                  <Image source={require('../../resources/images/icons/icon-nav_back-green.png')}/>
+              </TouchableOpacity>
+              <Text style={styles.title}>Tarefas</Text>
+              <View style={styles.emptyView}></View>
+
+          </View>
+          <FixedMenu />
       </View>
     );
   }
@@ -18,4 +34,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+    fixedNav: {
+        flexDirection: 'row',
+        height: height * 0.1,
+        width: width,
+        alignItems:'center',
+        justifyContent: 'space-between',
+        backgroundColor: '#F8F8F8',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        marginBottom: 20,
+        elevation: 3,
+        position: 'relative'
+    },
+
+    fixedNavArea: {
+        height: height * 0.08,
+        width: width * 0.15,
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+
+    emptyView:{
+        height: height * 0.08,
+        width: width * 0.15,
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+
+    title:{
+        fontFamily: 'Roboto',
+        fontSize: 20,
+        color: '#00ED74',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 });
