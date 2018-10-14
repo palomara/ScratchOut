6ce5461f9e55954dffb501de7631b6a4ea7d5cb6
palomara <palomarangelll@hotmail.com>
2018-10-14 01:17:34 -0300

    tasks flatList

--- App/sOut/index.js
+++ App/sOut/index.js
@@ -4,6 +4,7 @@ import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import Profile from './src/screens/Profile';
 import Hall from './src/screens/Hall';
+import TasksList from './src/screens/TasksList';
 import App from './src/App'
 
 import {name as appName} from './app.json';
@@ -13,3 +14,4 @@ AppRegistry.registerComponent(appName, () => App);
 //AppRegistry.registerComponent(appName, () => Login);
 //AppRegistry.registerComponent(appName, () => Hall);
 //AppRegistry.registerComponent(appName, () => Profile);
+//AppRegistry.registerComponent(appName, () => TasksList);
