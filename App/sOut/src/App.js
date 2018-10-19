56167c51638990ed7f0bc92e7208aaa7c48cc5bf
Eduardo <haevorak@gmail.com>
2018-10-18 21:16:34 -0300

    Merge

--- App/sOut/src/App.js
+++ App/sOut/src/App.js
@@ -34,11 +34,7 @@ const HomehallStack = createStackNavigator({
 
 const Application = createStackNavigator(
     {
-<<<<<<< HEAD
-        Home: {screen: Home},
-=======
         Splash: {screen: AuthOrApp},
->>>>>>> e9b4cf99466e39a71ef8c035c2b7d962c1cb7fe3
         Hall: LoginStack,
         Home: HomehallStack
 
